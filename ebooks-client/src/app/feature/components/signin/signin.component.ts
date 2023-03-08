3c9ef87052c7c24c9c8f2d9e2313aa73bd00f9ec
sebasruiz09 <sebasruiz909@gmail.com>
2023-03-08 11:58:23 -0500

    ADD : initial structure with i18n translation, main modules and standalone components

---
+++ ebooks-client/src/app/feature/components/signin/signin.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.sass'],
+})
+export class SigninComponent {}
---
+++ ebooks-client/src/app/feature/components/signup/signup.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone : true,
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.sass']
+})
+export class SignupComponent {
+
+}
